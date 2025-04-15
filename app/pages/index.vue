b869f9a9f57a551d437e32cecf20be4d13f93f1e
Bagas Yadher Bima Nugraha Ainur Rofiq Hidayat <bagas.ybnarh@gmail.com>
2025-04-15 13:19:44 +0700

    Initialized Nuxt with Tailwind, Color Mode, Kinde, and Hub

---
+++ app/pages/index.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+
+const colorMode = useColorMode()
+// const isDark = computed({
+//   get() {
+//     return colorMode.value === 'dark'
+//   },
+//   set() {
+//     colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
+//   },
+// })
+</script>
+
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+    <p class="bg-red-500 dark:bg-white dark:text-amber-400">Test</p>
+    <NuxtLink to="/api/login" external>
+      Sign in
+    </NuxtLink>
+    <NuxtLink to="/api/register" external>
+      Sign up
+    </NuxtLink>
+  </div>
+</template>
